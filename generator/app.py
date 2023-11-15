e2fe7b6b8d2c1cdcafd7bc7d02a874ca0bd1ce3c
Christopher Getschmann <christopher@volzo.de>
2023-11-15 12:07:47 +0100

    flask: file upload

--- generator/app.py
+++ generator/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from PIL import Image
 
 from . import generate
 
-from PIL import Image
 import io
 import base64
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -19,24 +20,35 @@ def home():
 @app.route("/upload", methods=["POST"])
 def upload():
 
-    uploaded_image = request.files["image"].read()
-    uploaded_image = Image.open(io.BytesIO(uploaded_image))
+    try:
 
-    # result = generate.generate(Image.open(generate.INPUT_IMAGE))
-    result = generate.generate(uploaded_image)
-    
-    img = result["image"]
+        uploaded_filename = request.files["image"].filename
 
-    img_bytes = io.BytesIO()
-    img.save(img_bytes, format="PNG")
-    img_base64 = base64.b64encode(img_bytes.getvalue())
+        uploaded_image = request.files["image"].read()
+        uploaded_image = Image.open(io.BytesIO(uploaded_image))
 
-    gcode_base64 = base64.b64encode(bytearray(result["gcode"], "utf-8"))
+        result = generate.generate(uploaded_image)
+        
+        img = result["image"]
 
-    data = {
-        "image_data": img_base64.decode("utf-8"),
-        "gcode": result["gcode"],
-        "gcode_base64": gcode_base64.decode("utf-8")
-    }
+        img_bytes = io.BytesIO()
+        img.save(img_bytes, format="PNG")
+        img_base64 = base64.b64encode(img_bytes.getvalue())
 
-    return render_template("result.html", data=data)
+        gcode_base64 = base64.b64encode(bytearray(result["gcode"], "utf-8"))
+
+        gcode_filename = Path(uploaded_filename).stem + ".gcode"
+
+        data = {
+            "image_filename": uploaded_filename,
+            "preview_image_data": img_base64.decode("utf-8"),
+            "gcode_filename": gcode_filename,
+            "gcode": result["gcode"],
+            "gcode_base64": gcode_base64.decode("utf-8")
+        }
+
+        return render_template("result.html", data=data)
+
+    except Exception as e:
+
+        return render_template("error.html")
